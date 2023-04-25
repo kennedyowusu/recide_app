8df2482cfbcadf8c678739fb7f9eb1f9b1dd965b
Kennedy Owusu <kennediowusu@gmail.com>
2023-04-25 22:25:58 +0000

    update

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 Route::get('/reset-password/{token}', function ($token) {
     return view('auth.reset-password', ['token' => $token]);
